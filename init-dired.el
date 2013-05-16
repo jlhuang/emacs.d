c7e0c6266ea339058d80dd29ac95e05a467acad6
jlhuang <jlhuang0821@gmail.com>
2013-05-16 23:55:04 +0900

    add the function that open file in external app within dired and bind it to "o"

--- init-dired.el
+++ init-dired.el
@@ -1,12 +1,21 @@
+(defun emacs-dired-open-file-in-external-app ()
+  "Open the current file in external app.
+   Works only in Mac OS X."
+  (interactive)
+  (when (string-equal system-type "darwin")
+        (funcall (lambda (filename) (shell-command (format "open \"%s\"" filename)))
+                 (dired-get-filename))))
+
 (eval-after-load 'dired
   '(progn
      (require 'dired+)
      (put 'dired-find-alternate-file 'disabled nil)
      (setq dired-recursive-deletes 'top)
-     ;;(define-key dired-mode-map [mouse-2] 'dired-find-file)
      (add-hook 'dired-mode-hook
                (lambda ()
                  (define-key dired-mode-map (kbd "^")
-                   (lambda () (interactive) (find-alternate-file "..")))))))
+                   (lambda () (interactive) (find-alternate-file "..")))
+                 (define-key dired-mode-map (kbd "o") 'emacs-dired-open-file-in-external-app) 
+                 ))))
 
 (provide 'init-dired)
