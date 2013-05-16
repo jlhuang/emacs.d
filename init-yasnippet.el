72b97d572e06394695cba455699662f660a8c8db
jlhuang <jlhuang0821@gmail.com>
2013-05-17 00:03:54 +0900

    delete the config that uses the dropdown-list package

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,18 +1,12 @@
 (require 'yasnippet)
-(if (fboundp 'yas/initialize)
-  (yas/initialize)
-  (yas-global-mode 1)
-  )
+
+;; use YASnippet as a non-global minor mode
+(yas-reload-all)
+(add-hook 'prog-mode-hook
+          '(lambda ()
+             (yas-minor-mode)))
+
 ;; default TAB key is occupied by auto-complete
 (global-set-key (kbd "C-c y") 'yas/expand)
-;; default hotkey `C-c & C-s` is still valid
-(global-set-key (kbd "C-c i") 'yas/insert-snippet)
-;; give yas/dropdown-prompt in yas/prompt-functions a chance
-(require 'dropdown-list)
-;; use yas/completing-prompt when ONLY when `M-x yas/insert-snippet'
-;; thanks to capitaomorte for providing the trick.
-(defadvice yas/insert-snippet (around use-completing-prompt activate)
-     "Use `yas/completing-prompt' for `yas/prompt-functions' but only here..."
-       (let ((yas/prompt-functions '(yas/completing-prompt)))
-             ad-do-it))
+
 (provide 'init-yasnippet)
